8a5c510ad6aa25c98a464dbcb0fe08b6122b17a6
meihua <meihua_1117@hotmail.com>
2024-10-09 17:28:58 +0900

    add ignore

--- lib/db_config.php
+++ lib/db_config.php
@@ -4,9 +4,14 @@ $mysql_host = 'localhost';
 $mysql_user = 'kiam';
 $mysql_password = 'only12!@db';
 $mysql_db = 'kiam';
-$self_con=mysql_connect($mysql_host,$mysql_user,$mysql_password) or die(mysql_error());
-mysql_select_db($mysql_db) or die(mysql_error());
-mysql_query("set names utf8");
+global $self_con;
+$self_con = mysqli_connect($mysql_host,$mysql_user,$mysql_password,$mysql_db) or die(mysqli_error($self_con));
+if (!$self_con) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+//mysql_select_db($mysql_db) or die(mysqli_error($self_con));
+mysqli_query($self_con, "set names utf8");
 $post_Data = '';
 $key = '';
 ?>
