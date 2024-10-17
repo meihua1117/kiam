a880d6831ff102b06b132bffcc1e65a61d4d1ce1
meihua <meihua_1117@hotmail.com>
2024-10-17 14:28:56 +0900

    delete excel class

--- excel_down/Classes/PHPExcel/Shared/Escher/DggContainer/BstoreContainer/BSE/rlatjd_fun.php
+++
@@ -1,8 +0,0 @@
-<?
-$mysql_host = 'localhost';
-$mysql_user = 'zymo_beautyp';
-$mysql_password = 'q5703915';
-$mysql_db = 'zymo_beautyp';
-mysql_connect($mysql_host,$mysql_user,$mysql_password)or die(mysqli_error($self_con));
-mysql_select_db($mysql_db)or die(mysqli_error($self_con));
-?>
