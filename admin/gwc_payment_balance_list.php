3e60b465a02ad308a0d06c01f70160ba2a717fda
meihua <meihua_1117@hotmail.com>
2024-10-21 15:35:17 +0900

    ccc25

--- admin/gwc_payment_balance_list.php
+++ admin/gwc_payment_balance_list.php
@@ -317,7 +317,7 @@ $search_month = $search_month ? sprintf("%02d", $search_month) : sprintf("%02d",
                                             FROM (SELECT * FROM Gn_Gwc_Order WHERE cash_prod_pay = 0 and seller_id != '' $searchStr group by seller_id ORDER BY id DESC) AS g 
                                             LEFT JOIN Gn_Member ON g.seller_id = Gn_Member.mem_id $searchCondition ";*/
                                 echo $query;
-                                if(!$self_con)
+                                if(is_null($self_con))
                                     echo "conn is null";
                                 $res        = mysqli_query($self_con, $query);
                                 echo "2";
