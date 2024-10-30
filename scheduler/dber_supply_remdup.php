6a3f856b1ccf3c34413334cbf860310116cbf867
meihua <meihua_1117@hotmail.com>
2024-10-30 12:37:06 +0900

    30

--- scheduler/dber_supply_remdup.php
+++ scheduler/dber_supply_remdup.php
@@ -2,74 +2,71 @@
 <?php
 
 set_time_limit(0);
-ini_set('memory_limit','-1');
+ini_set('memory_limit', '-1');
 
 $mysql_host = 'localhost';
 $mysql_user = 'kiam';
 $mysql_password = 'only12!@db';
 $mysql_db = 'kiam';
 
-$self_con=mysql_connect($mysql_host,$mysql_user,$mysql_password) or die(mysql_error());
-mysql_select_db($mysql_db) or die(mysql_error());
-mysql_query("set names utf8");
-
+$self_con = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_db);
+if (!$self_con) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+mysqli_query($self_con, "set names utf8");
 echo "Started working with duplication\n";
 
 $query = "start transaction";
-$result = mysql_query($query) ;
+$result = mysqli_query($self_con, $query);
 
 $totalPhoneNum = 0;
-while(true)
-{
+while (true) {
     $idxList = array();
-    $query = "select seq from crawler_data_supply where cell != '' GROUP BY cell HAVING count(seq) > 1";
-    $result = mysql_query($query) ;
-    while($row=mysql_fetch_array($result)) {
+    //$query = "select seq from crawler_data_supply where cell != '' GROUP BY cell HAVING count(seq) > 1";
+    $query = "SELECT MIN(seq) as seq FROM crawler_data_supply WHERE cell != '' GROUP BY cell HAVING COUNT(seq) > 1";
+    $result = mysqli_query($self_con, $query);
+    while ($row = mysqli_fetch_array($result)) {
         array_push($idxList, $row['seq']);
     }
 
-    if(count($idxList) == 0)
+    if (count($idxList) == 0)
         break;
-    
+
     $totalPhoneNum += count($idxList);
     echo "=====>Found " . count($idxList) . " duplicated phone numbers\n";
     $idx_group = "'" . implode("','", $idxList) . "'";
     $sub_sql = "delete from crawler_data_supply where seq in ($idx_group)";
-    mysql_query($sub_sql) ;
-
+    mysqli_query($self_con, $sub_sql);
 }
 
 echo "Removed $totalPhoneNum phone number duplication\n";
 
 $totalPhoneNum = 0;
-while(true)
-{
+while (true) {
     $idxList = array();
-    $query = "select seq from crawler_data_supply where cell ='' and email !='' GROUP BY email HAVING count(seq) > 1";
-    $result = mysql_query($query);
-    while($row=mysql_fetch_array($result)) {
+    //$query = "select seq from crawler_data_supply where cell ='' and email !='' GROUP BY email HAVING count(seq) > 1";
+    $query = "SELECT MIN(seq) AS seq FROM crawler_data_supply WHERE cell = '' AND email != '' GROUP BY email HAVING COUNT(seq) > 1";
+    $result = mysqli_query($self_con, $query);
+    while ($row = mysqli_fetch_array($result)) {
         array_push($idxList, $row['seq']);
     }
 
-    if(count($idxList) == 0)
-        break;   
+    if (count($idxList) == 0)
+        break;
 
     $totalPhoneNum += count($idxList);
     echo "=====>Found " . count($idxList) . " duplicated emails\n";
     $idx_group = "'" . implode("','", $idxList) . "'";
     $sub_sql = "delete from crawler_data_supply where seq in ($idx_group)";
-    mysql_query($sub_sql) ;
-    
-    
+    mysqli_query($self_con, $sub_sql);
 }
 
 echo "Removed $totalPhoneNum email duplication\n";
 
-
 $query = "commit";
-$result = mysql_query($query) ;
-
+$result = mysqli_query($self_con, $query);
 echo "Well Done!!!\n";
 
-mysql_close($self_con);
+mysqli_close($self_con);
 ?>
--- scheduler/dber_supply_update.php
+++ scheduler/dber_supply_update.php
@@ -1,76 +1,78 @@
 #!/opt/php/bin/php
 <?php
 set_time_limit(0);
-ini_set('memory_limit','-1');
+ini_set('memory_limit', '-1');
 
 $mysql_host = 'localhost';
 $mysql_user = 'kiam';
 $mysql_password = 'only12!@db';
 $mysql_db = 'kiam';
 
-$self_con=mysql_connect($mysql_host,$mysql_user,$mysql_password) or die(mysql_error());
-mysql_select_db($mysql_db) or die(mysql_error());
-mysql_query("set names utf8");
+$self_con = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_db);
+if (!$self_con) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+mysqli_query($self_con, "set names utf8");
 
 
 echo "Start working with daily update\n";
 
 $query = "start transaction";
-$result = mysql_query($query) ;
+$result = mysqli_query($self_con, $query);
 
 $sub_sql = "select end_idx from crawler_conf";
-$result1 = mysql_query($sub_sql);
-$row1=mysql_fetch_array($result1);
+$result1 = mysqli_query($self_con, $sub_sql);
+$row1 = mysqli_fetch_array($result1);
 $startIdx = $row1['end_idx'];
 
-echo "startIdx=". $startIdx . "\n";
+echo "startIdx=" . $startIdx . "\n";
 
 $length =   1000000;
-$endIdx = 0; 
+$endIdx = 0;
 $query = "select seq, user_id, keyword, incword, exword, data_type, cell, email, ceo, url, company_name, company_type, page_title, address, upload_date, tag, info, regdate from crawler_data_2022 where seq > $startIdx order by seq limit $length";
-$result = mysql_query($query) ;
-while($row=mysql_fetch_array($result)) {
+$result = mysqli_query($self_con, $query);
+while ($row = mysqli_fetch_array($result)) {
 
-    $title = str_replace("'","",$row['page_title']);	
-    $info = str_replace("'","",$row['info']);
-    $upload_date=($row['upload_date'] != '')?$row['upload_date'] : "0000-00-00";
-    $keyword = str_replace("\n","",$row['keyword']);
+    $title = str_replace("'", "", $row['page_title']);
+    $info = str_replace("'", "", $row['info']);
+    $upload_date = ($row['upload_date'] != '') ? $row['upload_date'] : "0000-00-00";
+    $keyword = str_replace("\n", "", $row['keyword']);
     $keyword = trim($keyword);
 
-    $query = "insert into crawler_data_supply set user_id='$row[user_id]',
-    keyword='$keyword',
-    incword='$row[incword]',
-    exword='$row[exword]',
-    data_type='$row[data_type]',
-    cell='$row[cell]',
-    email='$row[email]',
-    ceo='$row[ceo]',
-    url='$row[url]',
-    company_name='$row[company_name]',
-    company_type='$row[company_type]',
-    page_title='$title',
-    address='$row[address]',
-    upload_date='$upload_date',
-    tag='$row[tag]',
-    info='$info',
-    regdate='$row[regdate]'";
-    mysql_query($query);
+    $query = "insert into crawler_data_supply set user_id='{$row['user_id']}',
+                                                keyword='$keyword',
+                                                incword='{$row['incword']}',
+                                                exword='{$row['exword']}',
+                                                data_type='{$row['data_type']}',
+                                                cell='{$row['cell']}',
+                                                email='{$row['email']}',
+                                                ceo='{$row['ceo']}',
+                                                url='{$row['url']}',
+                                                company_name='{$row['company_name']}',
+                                                company_type='{$row['company_type']}',
+                                                page_title='$title',
+                                                address='{$row['address']}',
+                                                upload_date='$upload_date',
+                                                tag='{$row['tag']}',
+                                                info='$info',
+                                                regdate='{$row['regdate']}'";
+    mysqli_query($self_con, $query);
 
     //echo $query . "\n";
     $endIdx = $row['seq'];
 }
 
-echo "endIdx=". $endIdx. "\n";
+echo "endIdx=" . $endIdx . "\n";
 
-if($endIdx > 0)
-{
+if ($endIdx > 0) {
     $query = "update crawler_conf set end_idx='$endIdx'";
-    mysql_query($query);
+    mysqli_query($self_con, $query);
 }
 
 $query = "commit";
-$result = mysql_query($query);
+$result = mysqli_query($self_con, $query);
 
 echo "Well Done !!!\n";
-mysql_close($self_con);
+mysqli_close($self_con);
 ?>
