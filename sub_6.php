608d50222ce7bff3fde5e06f63cc28eaa0611380
meihua <meihua_1117@hotmail.com>
2024-11-01 10:34:53 +0900

    1101

--- sub_6.php
+++ sub_6.php
@@ -1305,12 +1305,12 @@ if (!empty($row_sum_b)) {
 										</thead>
 										<tbody id="phone_list">
 											<?
-											echo $intRowCount;
 											if ($intRowCount) {
 												$today_send_total = 0;
 												$use_phone_cnt = 0;
 												$today_reg = date("Y-m-d");
 												while ($row = mysqli_fetch_array($resul)) {
+													echo "test1313";
 													$row['user_cnt'] = $row['daily_limit_cnt_user'];
 													$is_send = true;
 													$sql_result2_g = "select SUM(recv_num_cnt) from Gn_MMS where send_num='{$row['sendnum']}' and ((reg_date like '$today_reg%' and reservation is null) or reservation like '$today_reg%')";
@@ -1337,6 +1337,7 @@ if (!empty($row_sum_b)) {
 															$row['user_cnt'] =     $row['daily_limit_cnt_user'] - $today_cnt_1;
 														}
 													}
+													echo "test1339";
 													if ($row['user_cnt'] < 0) $row['user_cnt'] = 0;
 													// =========== Cooper add 폰별 월 발송량 체크  Start ===========
 													// $query = "select * from Gn_MMS_Number where mem_id='{$_SESSION['one_member_id']}' and sendnum='".$row['sendnum']."'";
@@ -1378,7 +1379,7 @@ if (!empty($row_sum_b)) {
 													// 당일 발송회수가 1이면 한개를 더 차감으로 커멘트함
 													// if($today_cnt_1 == 1)
 													// 	$row['user_cnt'] = $row['user_cnt'] - $today_cnt_1;
-
+													echo "test1382";
 													if ($member_1['mem_type'] == "V" && $mem_phone == $row['sendnum']) {
 														$is_send = true;
 														//$today_send_total+=$row['user_cnt'];
@@ -1404,6 +1405,7 @@ if (!empty($row_sum_b)) {
 													$resul_s = mysqli_query($self_con, $sql_s);
 													$row_s = mysqli_fetch_array($resul_s);
 													mysqli_free_result($resul_s);
+													echo "test1407";
 													if ($row_s['status'] == "-1") {
 														$rdate = date("Y-m-d", strtotime($row_s['regdate']));
 														if ($rdate == $today_reg) {
@@ -1416,6 +1418,7 @@ if (!empty($row_sum_b)) {
 													}
 													if ($is_send == true)
 														$use_phone_cnt++;
+														echo "test1420";
 											?>
 													<tr style="<?php if ($row_s['status'] == "-1") {
 																	echo "background:#efefef";
