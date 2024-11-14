187ff024b2666ee2ec336ec3da735b75a46739a5
meihua <meihua_1117@hotmail.com>
2024-11-14 15:29:51 +0900

    14

--- pay.php
+++ pay.php
@@ -21,6 +21,7 @@ $show_iam_info_status = "N";
 $query = "select count(no) from tjd_pay_result where buyer_id='{$member_1['mem_id']}' and
           ((member_type like '%standard%' or member_type like '%professional%' or member_type like '%enterprise%') or 
           (((iam_pay_type = '' or iam_pay_type = '0' or iam_pay_type = '전문가') and member_type != '포인트충전')) and end_status = 'Y'";
+echo $query;
 $res = mysqli_query($self_con,$query);
 $pay_row = mysqli_fetch_array($res);
 echo "test25<br>";
