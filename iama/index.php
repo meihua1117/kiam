8fb0af57fbceb8543d4691505094f360e570efa3
meihua <meihua_1117@hotmail.com>
2024-11-18 16:02:11 +0900

    1118

--- iama/index.php
+++ iama/index.php
@@ -64,6 +64,7 @@ $row = mysqli_fetch_array($res);
 $contents_new_cnt = $row[0];
 echo "test9"."<br>";
 $query = "select sum(iam_share) from Gn_Iam_Contents a inner join  Gn_Member b on a.mem_id = b.mem_id  where b.site_iam='$site[0]' ";
+echo $query;
 $res = mysqli_query($self_con, $query);
 $row = mysqli_fetch_array($res);
 $contents_share_cnt = $row[0];
