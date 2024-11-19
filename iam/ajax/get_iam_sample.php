2beee28b065e44bad0e6a02181c6401565308312
meihua <meihua_1117@hotmail.com>
2024-11-19 10:44:39 +0900

    redis

--- iam/ajax/get_iam_sample.php
+++ iam/ajax/get_iam_sample.php
@@ -64,8 +64,8 @@ else if($_GET['sample_type'] == "recent_sample")
 }
 $redisCache = new RedisCache();
 //$redisCache->set_debug(true);
-$sample_list[] = $redisCache -> get_query_to_array($sql8);
-$logs->add_log( $redisCache ->get_debug_string(), false);
+$sample_list = $redisCache -> get_query_to_array($sql8);
+//$logs->add_log( $redisCache ->get_debug_string(), false);
 $body = '';
 for($i=0 ; $i < count($sample_list); $i++)
 {
