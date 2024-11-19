a8adcc8741f22eaaab1570655045e292a73458b7
meihua <meihua_1117@hotmail.com>
2024-11-19 10:41:33 +0900

    sample

--- iam/ajax/get_iam_sample.php
+++ iam/ajax/get_iam_sample.php
@@ -69,8 +69,8 @@ $logs->add_log( $redisCache ->get_debug_string(), false);
 $body = '';
 for($i=0 ; $i < count($sample_list); $i++)
 {
-    $body .= count($sample_list);
     $contents_row = $sample_list[$i];
+    $body .= json_encode($contents_row);
     $sample_sql = "select mem_code from Gn_Member where mem_id='{$contents_row['mem_id']}'";
     $sample_res = mysqli_query($self_con,$sample_sql);
     $sample_row = mysqli_fetch_array($sample_res);
