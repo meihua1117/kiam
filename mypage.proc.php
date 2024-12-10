3709b8c6b178d7b55fb2cf015e40b4bd79564088
meihua <meihua_1117@hotmail.com>
2024-12-10 15:14:46 +0900

    디비예약

--- mypage.proc.php
+++ mypage.proc.php
@@ -6,8 +6,8 @@ function IntervalDays($CheckIn, $CheckOut)
 {
     $CheckInX = explode("-", $CheckIn);
     $CheckOutX =  explode("-", $CheckOut);
-    $date1 =  mktime(0, 0, 0, $CheckInX[1], $CheckInX[2], $CheckInX[0]);
-    $date2 =  mktime(0, 0, 0, $CheckOutX[1], $CheckOutX[2], $CheckOutX[0]);
+    $date1 =  time(0, 0, 0, $CheckInX[1], $CheckInX[2], $CheckInX[0]);
+    $date2 =  time(0, 0, 0, $CheckOutX[1], $CheckOutX[2], $CheckOutX[0]);
     $interval = ($date2 - $date1) / (3600 * 24);
     return  $interval;
 }
