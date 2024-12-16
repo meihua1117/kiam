07b7ce431bf2395a438cd925abc213d800b5cc0d
meihua <meihua_1117@hotmail.com>
2024-12-16 11:48:55 +0900

    g.php

--- mms/receive_status_end.php
+++ mms/receive_status_end.php
@@ -2,7 +2,6 @@
 // header("Content-type: text/html; charset=utf-8");
 include_once $_SERVER['DOCUMENT_ROOT'] . "/lib/db_config.php";
 include_once $_SERVER['DOCUMENT_ROOT'] . "/lib/common_func.php";
-$fp = fopen("receive_status_end.log", "w+");
 /*
 수신번호별 발송상태를 
 IN
@@ -35,14 +34,12 @@ $query = "insert into GN_MMS_status_log set idx='{$idx[0]}',
                                                 recv_num='{$_POST['recv_num']}',
                                                 status='{$status[0]}',
                                                 end_time='{$end_time[0]}'";
-fwrite($fp, $query . "\r\n");
 mysqli_query($self_con, $query) or die(mysqli_error($self_con));
 if ($_POST['idx'] == "")  exit;
 if ($_POST['send_num'] == "")  exit;
 
 if (count($recv_num) == 1) {
     $sql = "select idx from Gn_MMS_status where idx='{$_POST['idx']}' and send_num='{$_POST['send_num']}' and recv_num='{$_POST['recv_num']}'";
-    fwrite($fp, $sql . "\r\n");
     $resul = mysqli_query($self_con, $sql) or die(mysqli_error($self_con));
     $row = mysqli_fetch_array($resul);
     if ($row['idx'] == "") {
@@ -51,7 +48,6 @@ if (count($recv_num) == 1) {
                                                              recv_num='{$_POST['recv_num']}',
                                                              status='{$_POST['status']}',
                                                              regdate='{$_POST['end_time']}'";
-        fwrite($fp, $sql_insert . "\r\n");
         mysqli_query($self_con, $sql_insert);
     }
 } else {
@@ -65,7 +61,6 @@ if (count($recv_num) == 1) {
                                                                  recv_num='{$recv_num[$i]}',
                                                                  status='{$status[$i]}',
                                                                  regdate='{$end_time[$i]}'";
-            fwrite($fp, "69:" . $sql_insert . "\r\n");
             mysqli_query($self_con, $sql_insert);
         }
     }
@@ -83,11 +78,9 @@ if (count($send_arr)) {
         $row = mysqli_fetch_array($res);
         if ($row['idx'] != "") {
             $sql = "update call_api_log set receive_status_end='{$time}' where idx='{$row['idx']}'";
-            fwrite($fp, $sql . "\r\n");
             mysqli_query($self_con, $sql);
         } else {
             $sql = "insert into call_api_log set receive_status_end='{$time}', phone_num='{$phone_num}'";
-            fwrite($fp, $sql . "\r\n");
             mysqli_query($self_con, $sql);
         }
     }
