f42d865b7b6d3dedff1d311dc492b4975cd22df2
meihua <meihua_1117@hotmail.com>
2024-12-17 09:19:48 +0900

    u

--- u.php
+++ u.php
@@ -107,6 +107,7 @@ if ($_REQUEST['mode'] == "inser") {
 
     // [새 번호]가 현재로그에 있는지 확인
     $query = "select now_num, old_nums from Gn_MMS_Receive_Change_Log where  old_nums like '$now_num%' ";
+    echo $query."<br>";
     $resultA = mysqli_query($self_con, $query);
     $rowA = mysqli_fetch_array($resultA);
     if ($rowA[0]) {
