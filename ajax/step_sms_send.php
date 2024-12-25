0b47ab71b84c1f9e1c939c68138aad1836161384
meihua <meihua_1117@hotmail.com>
2024-12-25 09:47:57 +0900

    sms

--- ajax/step_sms_send.php
+++ ajax/step_sms_send.php
@@ -1,6 +1,7 @@
 <?
 include_once "../lib/rlatjd_fun.php";
 $GLOBALS['host'] = $HTTP_HOST;
+$fp = fopen("step_sms_send".date("YmdHis").".log","w+");
 if (isset($_POST['send_ids'])) {
   $ids = $_POST['send_ids'];
   if ($_POST['type'] == "step") {
@@ -12,26 +13,25 @@ if (isset($_POST['send_ids'])) {
         if (strpos($sms_idx, ",") !== false) {
           $sms_idx_arr = explode(",", $sms_idx);
           for ($j = 0; $j < count($sms_idx_arr); $j++) {
-            insert_db($sms_idx_arr[$j], $ids_arr[$i]);
+            insert_db($sms_idx_arr[$j], $ids_arr[$i],$fp);
           }
         } else {
-          insert_db($sms_idx, $ids_arr[$i]);
+          insert_db($sms_idx, $ids_arr[$i],$fp);
         }
       }
     } else {
       if (strpos($sms_idx, ",") !== false) {
         $sms_idx_arr = explode(",", $sms_idx);
         for ($j = 0; $j < count($sms_idx_arr); $j++) {
-          insert_db($sms_idx_arr[$j], $ids);
+          insert_db($sms_idx_arr[$j], $ids,$fp);
         }
       } else {
-        insert_db($sms_idx, $ids);
+        insert_db($sms_idx, $ids,$fp);
       }
     }
 
     $sql_send_ids = "update Gn_Member set step_send_ids='{$ids}' where mem_id='{$_SESSION['one_member_id']}'";
-    mysqli_query($self_con,$sql_send_ids);
-
+    mysqli_query($self_con, $sql_send_ids);
     echo 1;
   } else if ($_POST['type'] == "reqlink") {
     $req_idx = $_POST['req_idx'];
@@ -41,20 +41,20 @@ if (isset($_POST['send_ids'])) {
         if (strpos($req_idx, ",") !== false) {
           $req_idx_arr = explode(",", $req_idx);
           for ($j = 0; $j < count($req_idx_arr); $j++) {
-            insert_db_reqlink($req_idx_arr[$j], $ids_arr[$i]);
+            insert_db_reqlink($req_idx_arr[$j], $ids_arr[$i],$fp);
           }
         } else {
-          insert_db_reqlink($req_idx, $ids_arr[$i]);
+          insert_db_reqlink($req_idx, $ids_arr[$i],$fp);
         }
       }
     } else {
       if (strpos($req_idx, ",") !== false) {
         $req_idx_arr = explode(",", $req_idx);
         for ($j = 0; $j < count($req_idx_arr); $j++) {
-          insert_db_reqlink($req_idx_arr[$j], $ids);
+          insert_db_reqlink($req_idx_arr[$j], $ids,$fp);
         }
       } else {
-        insert_db_reqlink($req_idx, $ids);
+        insert_db_reqlink($req_idx, $ids,$fp);
       }
     }
   } else if ($_POST['type'] == "landlink") {
@@ -65,20 +65,20 @@ if (isset($_POST['send_ids'])) {
         if (strpos($land_idx, ",") !== false) {
           $land_idx_arr = explode(",", $land_idx);
           for ($j = 0; $j < count($land_idx_arr); $j++) {
-            insert_db_landlink($land_idx_arr[$j], $ids_arr[$i]);
+            insert_db_landlink($land_idx_arr[$j], $ids_arr[$i],$fp);
           }
         } else {
-          insert_db_landlink($land_idx, $ids_arr[$i]);
+          insert_db_landlink($land_idx, $ids_arr[$i],$fp);
         }
       }
     } else {
       if (strpos($land_idx, ",") !== false) {
         $land_idx_arr = explode(",", $land_idx);
         for ($j = 0; $j < count($land_idx_arr); $j++) {
-          insert_db_landlink($land_idx_arr[$j], $ids);
+          insert_db_landlink($land_idx_arr[$j], $ids,$fp);
         }
       } else {
-        insert_db_landlink($land_idx, $ids);
+        insert_db_landlink($land_idx, $ids,$fp);
       }
     }
   }
@@ -96,34 +96,36 @@ if (isset($_POST['send_ids'])) {
     $img = "image2";
   }
   $sql_update = "update Gn_event_sms_step_info set " . $img . "='' where sms_detail_idx={$sms_detail_idx}";
-  mysqli_query($self_con,$sql_update);
+  mysqli_query($self_con, $sql_update);
   echo 1;
 }
 
-function insert_db($idx, $mem_id)
+function insert_db($idx, $mem_id,$fp)
 {
   global $self_con;
   $date = time();
   $num = rand(10, 90);
   $event_name_eng = $date . $num;
   $sql_mem_chk = "select count(mem_code) from Gn_Member where mem_id='{$mem_id}'";
-  $res_mem_chk = mysqli_query($self_con,$sql_mem_chk);
+  $res_mem_chk = mysqli_query($self_con, $sql_mem_chk);
   $row_mem_chk = mysqli_fetch_array($res_mem_chk);
   if ($row_mem_chk[0]) {
     $sql = "INSERT INTO Gn_event_sms_info (event_idx, event_name_eng, mobile, reservation_title, reservation_desc, m_id, regdate)
                       (SELECT event_idx, '{$event_name_eng}', mobile, reservation_title, reservation_desc, '{$mem_id}', now() FROM Gn_event_sms_info where sms_idx={$idx})";
-    mysqli_query($self_con,$sql);
+    fwrite($fp,"115:".$sql."\r\n");
+    mysqli_query($self_con, $sql);
     $sms_idx = mysqli_insert_id($self_con);
     $sql_step_info = "INSERT INTO Gn_event_sms_step_info (sms_idx, step, send_day, send_time, title, content, image, image1, image2, regdate, send_deny) 
                                 (SELECT {$sms_idx}, step, send_day, send_time, title, content, image, image1, image2, now(), send_deny FROM Gn_event_sms_step_info WHERE sms_idx={$idx})";
-    mysqli_query($self_con,$sql_step_info);
+    fwrite($fp,"120:".$sql_step_info."\r\n");
+    mysqli_query($self_con, $sql_step_info);
     return $sms_idx;
   } else {
     return 0;
   }
 }
 
-function insert_db_reqlink($idx, $mem_id)
+function insert_db_reqlink($idx, $mem_id,$fp)
 {
   global $self_con;
   $date = time();
@@ -131,14 +133,15 @@ function insert_db_reqlink($idx, $mem_id)
   $pcode = $event_name_eng = $date . $num;
 
   $sql_mem_chk = "select * from Gn_Member where mem_id='{$mem_id}'";
-  $res_mem_chk = mysqli_query($self_con,$sql_mem_chk);
+  $res_mem_chk = mysqli_query($self_con, $sql_mem_chk);
   $row_mem_chk = mysqli_num_rows($res_mem_chk);
   if ($row_mem_chk) {
     $mem_data = mysqli_fetch_array($res_mem_chk);
     $phone_num = $mem_data['mem_phone'];
 
     $sql = "SELECT event_name_kor, event_title, event_desc, event_info, event_sms_desc, event_type, ip_addr, cnt, object, callback_no, event_req_link, daily_req_link,sms_idx1,sms_idx2,sms_idx3 FROM Gn_event where event_idx={$idx}";
-    $event_res = mysqli_query($self_con,$sql);
+    fwrite($fp,"143:".$sql."\r\n");
+    $event_res = mysqli_query($self_con, $sql);
     $event_row = mysqli_fetch_assoc($event_res);
     $sms_idx1 = $sms_idx2 = $sms_idx3 = 0;
     if ($event_row['sms_idx1'] != 0)
@@ -170,41 +173,43 @@ function insert_db_reqlink($idx, $mem_id)
                                 sms_idx1 = '{$sms_idx1}',
                                 sms_idx2 = '{$sms_idx2}',
                                 sms_idx3 = '{$sms_idx3}'";
-    mysqli_query($self_con,$sql);
+                                fwrite($fp,"176:".$sql."\r\n");
+    mysqli_query($self_con, $sql);
 
     $event_idx = mysqli_insert_id($self_con);
     $transUrl = "http://" . $GLOBALS['host'] . "/event/event.html?pcode=" . $pcode . "&sp=" . $event_name_eng;
     $transUrl = get_short_url($transUrl);
     $insert_short_url = "update Gn_event set short_url='{$transUrl}' where event_idx={$event_idx}";
-    mysqli_query($self_con,$insert_short_url) or die(mysqli_error($self_con));
+    mysqli_query($self_con, $insert_short_url) or die(mysqli_error($self_con));
   }
   return $pcode;
 }
 
-function insert_db_landlink($idx, $mem_id)
+function insert_db_landlink($idx, $mem_id,$fp)
 {
   global $self_con;
   $sql_mem_chk = "select * from Gn_Member where mem_id='{$mem_id}'";
-  $res_mem_chk = mysqli_query($self_con,$sql_mem_chk);
+  $res_mem_chk = mysqli_query($self_con, $sql_mem_chk);
   $row_mem_chk = mysqli_num_rows($res_mem_chk);
   if ($row_mem_chk) {
     $sp = "";
     $mem_data = mysqli_fetch_array($res_mem_chk);
     $phone_num = $mem_data['mem_phone'];
     $sql_land_data = "select * from Gn_landing where landing_idx={$idx}";
-    $res_land_data = mysqli_query($self_con,$sql_land_data);
+    $res_land_data = mysqli_query($self_con, $sql_land_data);
     $row_land_data = mysqli_fetch_array($res_land_data);
 
     if ($row_land_data['pcode']) {
       $sql = "select event_idx from Gn_event where pcode='{$row_land_data['pcode']}'";
-      $result = mysqli_query($self_con,$sql) or die(mysqli_error($self_con));
+      $result = mysqli_query($self_con, $sql) or die(mysqli_error($self_con));
       $event_data = mysqli_fetch_array($result);
       if ($event_data)
         $sp = insert_db_reqlink($event_data['event_idx'], $mem_id);
       //$sp = $event_data['pcode'];
     }
     $sql = "SELECT title, description, content, movie_url, footer_content, file, alarm_sms_yn, reply_yn, request_yn, pcode, status_yn, lecture_yn, cnt, event_idx FROM Gn_landing where landing_idx={$idx}";
-    $result = mysqli_query($self_con,$sql);
+    fwrite($fp,"211:".$sql."\r\n");
+    $result = mysqli_query($self_con, $sql);
     $land_row = mysqli_fetch_assoc($result);
 
     $sql = "INSERT INTO Gn_landing set title = '{$land_row['title']}', 
@@ -224,16 +229,18 @@ function insert_db_landlink($idx, $mem_id)
                                       lecture_yn = '{$land_row['lecture_yn']}', 
                                       cnt = '{$land_row['cnt']}', 
                                       m_id = '{$mem_id}'";
-    if($land_row['event_idx'] != "")
+                                      fwrite($fp,"232:".$sql."\r\n");
+    if ($land_row['event_idx'] != "")
       $sql .= ",event_idx = '{$land_row['event_idx']}'";
-    $result = mysqli_query($self_con,$sql);
+    $result = mysqli_query($self_con, $sql);
 
     $land_idx = mysqli_insert_id($self_con);
     $transUrl = "http://" . $GLOBALS['host'] . "/event/event.html?pcode=" . $row_land_data['pcode'] . "&sp=" . $sp . "&landing_idx=" . $land_idx;
     $transUrl = get_short_url($transUrl);
     $insert_short_url = "update Gn_landing set short_url='{$transUrl}' where landing_idx={$land_idx}";
-    mysqli_query($self_con,$insert_short_url) or die(mysqli_error($self_con));
+    mysqli_query($self_con, $insert_short_url) or die(mysqli_error($self_con));
   } else {
     return;
   }
 }
+?>
